0989be9e44466d392413fa2520108aafd879c117
h571569 <571569@stud.hvl.no>
2024-11-07 18:09:36 +0100

    java

--- src/main/java/no/hvl/dat100/oppgave2/Bilde.java
+++ src/main/java/no/hvl/dat100/oppgave2/Bilde.java
@@ -4,86 +4,30 @@ import no.hvl.dat100.common.TODO;
 
 public class Bilde extends Tekst {
 
-	private int id;
-	private String bruker;
-	private String dato;
-	private String tekst;
 	private String url;
-	private int likes;
 	
 	public Bilde(int id, String bruker, String dato, String tekst, String url) {
-		this.id = id;
-		this.bruker = bruker;
-		this.dato = dato;
-		this.tekst = tekst;
+		super(id, bruker, dato, tekst);
 		this.url = url;
 	}
 
-	@Override
-	public int getId() {
-		return id;
-	}
-
-	@Override
-	public String getTekst() {
-		return tekst;
-	}
-
-	@Override
-	public int getLikes() {
-		return likes;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	@Override
-	public void setBruker(String bruker) {
-		this.bruker = bruker;
-	}
-
-	@Override
-	public void setDato(String dato) {
-		this.dato = dato;
-	}
-
-	@Override
-	public void setTekst(String tekst) {
-		this.tekst = tekst;
-	}
-
-	public void setLikes(int likes) {
-		this.likes = likes;
-	}
 
 	public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
-		this.id = id;
-		this.bruker = bruker;
-		this.dato = dato;
-		this.likes = likes;
-		this.tekst = tekst;
+		super(id, bruker, dato, likes, tekst);
 		this.url = url;
 	}
 	
 	public String getUrl() {
 		return this.url;
-
 	}
 
 	public void setUrl(String url) {
 		this.url = url;
 	}
-	public String getBruker() {
-		return this.bruker;
-	}
-	public String getDato() {
-		return this.dato;
-	}
 
 	@Override
 	public String toString() {
-		return "BILDE\n" + this.id + "\n" + this.bruker + "\n" + this.dato + "\n" + this.likes + "\n" + this.tekst  + "\n" + this.url +"\n";
+		return "BILDE\n" + super.toString() + this.url +"\n";
 
 
 	}
--- src/main/java/no/hvl/dat100/oppgave2/Tekst.java
+++ src/main/java/no/hvl/dat100/oppgave2/Tekst.java
@@ -5,86 +5,35 @@ import no.hvl.dat100.oppgave1.*;
 
 public class Tekst extends Innlegg {
 
-
-	protected int likes;
-	protected int id;
-	protected String bruker;
 	protected String tekst;
-	protected String dato;
-
-
-	@Override
-	public String getDato() {
-		return dato;
-	}
-
-	@Override
-	public int getId() {
-		return id;
-	}
-
-	@Override
-	public String getBruker() {
-		return bruker;
-	}
-
-	public String getTekst() {
-		return tekst;
-	}
-
-	@Override
-	public int getLikes() {
-		return likes;
-	}
-
-	@Override
-	public void setDato(String dato) {
-		this.dato = dato;
-	}
-
-	public void setTekst(String tekst) {
-		this.tekst = tekst;
-	}
-
-	public void setLikes(int likes) {
-		this.likes = likes;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	@Override
-	public void setBruker(String bruker) {
-		this.bruker = bruker;
-	}
-
-
 	
 	public Tekst () {
 		
 	}
 	
 	public Tekst(int id, String bruker, String dato, String tekst) {
-		this.id = id;
-		this.bruker = bruker;
-		this.dato = dato;
+		super(id, bruker, dato);
 		this.tekst = tekst;
 	}
 	
 	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
-		this.id = id;
-		this.bruker = bruker;
-		this.dato = dato;
+		super(id, bruker, dato, likes);
 		this.tekst = tekst;
-		this.likes = likes;
 	}
-	
+
+
+	public String getTekst() {
+		return this.tekst;
+	}
+
+	public void setTekst(String tekst) {
+		this.tekst = tekst;
+	}
 
 
 	@Override
 	public String toString() {
-		return "TEKST\n" + this.id + "\n" + this.bruker + "\n" + this.dato + "\n" + this.likes + "\n" + this.tekst  + "\n";
+		return "TEKST\n" + super.toString() + this.tekst  + "\n";
 
 
 	}
