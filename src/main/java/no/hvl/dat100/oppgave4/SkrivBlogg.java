87a2aacaba1463a8e6893958b75db2f40b40232d
h571569 <571569@stud.hvl.no>
2024-11-12 13:22:41 +0100

    oppgave 4

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -1,7 +1,9 @@
 package no.hvl.dat100.oppgave4;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave3.*;
@@ -10,6 +12,49 @@ public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+
+		// METODE 1
+
+//		String folderPath = Paths.get(mappe, filnavn).toString();
+//		java.nio.file.Path path = Paths.get(folderPath);
+//
+//		try {
+//			if (!java.nio.file.Files.exists(path)) {
+//				java.nio.file.Files.createDirectories(path);
+//			}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		try (BufferedWriter writer = new BufferedWriter(new FileWriter(folderPath))) {
+//			writer.write(samling.toString());
+//			writer.newLine();
+//			return true;
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			return false;
+//		}
+
+
+
+
+		//METODE 2
+		
+		Path folderPath = Paths.get(mappe);
+		Path filePath = Paths.get(filnavn);
+
+		try {
+			Files.createDirectories(folderPath);
+
+			try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath.toFile()))) {
+				writer.write(samling.toString());
+				writer.newLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 }
